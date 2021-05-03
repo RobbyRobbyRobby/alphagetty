38ae041d234598ad582d3e9a9d90e67b1a572726
Rob <rlmartain@hotmail.com>
2021-05-03 15:04:40 +1000

    Re-sized background images for levels, and made a main menu background image

--- tilemap.g.ts
+++ tilemap.g.ts
@@ -3,17 +3,21 @@ namespace myTiles {
     //% fixedInstance jres blockIdentity=images._tile
     export const transparency16 = image.ofBuffer(hex``);
     //% fixedInstance jres blockIdentity=images._tile
-    export const tile2 = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=images._tile
     export const tile1 = image.ofBuffer(hex``);
     //% fixedInstance jres blockIdentity=images._tile
     export const tile3 = image.ofBuffer(hex``);
     //% fixedInstance jres blockIdentity=images._tile
-    export const tile4 = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=images._tile
     export const tile5 = image.ofBuffer(hex``);
     //% fixedInstance jres blockIdentity=images._tile
     export const tile6 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile4 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile2 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile7 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile8 = image.ofBuffer(hex``);
 
     helpers._registerFactory("tilemap", function(name: string) {
         switch(helpers.stringTrim(name)) {
@@ -517,18 +521,22 @@ namespace myTiles {
     helpers._registerFactory("tile", function(name: string) {
         switch(helpers.stringTrim(name)) {
             case "transparency16":return transparency16;
-            case "DeathTile":
-            case "tile2":return tile2;
             case "FoodStartingLocationAsset":
             case "tile1":return tile1;
             case "CoinStartingLocation":
             case "tile3":return tile3;
-            case "EnemyStartingLocation":
-            case "tile4":return tile4;
             case "GoalLocationTile":
             case "tile5":return tile5;
             case "PowerupStartingLocation":
             case "tile6":return tile6;
+            case "Enemy1StartingLocation":
+            case "tile4":return tile4;
+            case "Trap1Location":
+            case "tile2":return tile2;
+            case "Trap2Location":
+            case "tile7":return tile7;
+            case "Enemy2StartingLocation":
+            case "tile8":return tile8;
         }
         return null;
     })
